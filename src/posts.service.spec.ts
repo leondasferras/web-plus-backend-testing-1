3d0e573b0d9be1b1e5f427c9dd6d3f75bb5364f5
leondasferras <90497102+leondasferras@users.noreply.github.com>
2023-03-19 20:07:38 +0500

    123

--- src/posts.service.spec.ts
+++ src/posts.service.spec.ts
@@ -1,22 +1,25 @@
-import { Post, PostsService } from './posts.service';
+import { Post, PostsService } from "./posts.service";
 
-describe('PostsService', () => {
+describe("PostsService", () => {
   let postsService: PostsService;
-  const post: Omit<Post, 'id' | 'date'> = {
-    text: 'Mocked post',
+  const post: Omit<Post, "id" | "date"> = {
+    text: "Mocked post",
   };
 
   beforeEach(async () => {
     postsService = new PostsService();
-
-    postsService.create({ text: 'Some pre-existing post' });
+    postsService.create({ text: "Some pre-existing post" });
   });
 
-  it('should add a new post', () => {
-    // реализуйте тест-кейс
+  it("should add a new post", () => {
+
+    postsService.create(post)
+    expect(postsService.find('2')).toEqual({...post, id:'2', date: expect.any(String)})
+
   });
 
-  it('should find a post', () => {
-    // реализуйте тест-кейс
+  it("should find a post", () => {
+    postsService.create(post)
+    expect(postsService.find('2')).toEqual({...post, id:'2', date: expect.any(String)})
   });
 });
